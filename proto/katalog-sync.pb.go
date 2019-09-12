ca18a2b55685717acbb67ae69a1823c7e40706b3
Thomas Jackson <jacksontj.89@gmail.com>
2019-09-12 11:44:03 -0700

    goimports

--- proto/katalog-sync.pb.go
+++ proto/katalog-sync.pb.go
@@ -15,16 +15,19 @@
 */
 package katalogsync
 
-import proto "github.com/gogo/protobuf/proto"
-import fmt "fmt"
-import math "math"
-
 import (
-	context "golang.org/x/net/context"
-	grpc "google.golang.org/grpc"
-)
+	fmt "fmt"
 
-import io "io"
+	proto "github.com/gogo/protobuf/proto"
+
+	math "math"
+
+	context "golang.org/x/net/context"
+
+	grpc "google.golang.org/grpc"
+
+	io "io"
+)
 
 // Reference imports to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
