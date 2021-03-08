fbcafe730828bf81a339e9028295af85491172cc
Thomas Jackson <jacksontj.89@gmail.com>
2021-03-08 10:55:24 -0800

    make imports

--- pkg/daemon/daemon.go
+++ pkg/daemon/daemon.go
@@ -365,8 +365,8 @@ func (d *Daemon) syncConsul() error {
 			} else {
 				// Define the base metadata that katalog-sync requires
 				meta := map[string]string{
-					"external-source":    "kubernetes",            // Define the source of this service; see https://github.com/hashicorp/consul/blob/fc1d9e5d78749edc55249e5e7c1a8f7a24add99d/website/source/docs/platform/k8s/service-sync.html.md#service-meta
-					ConsulSyncSourceName: ConsulSyncSourceValue,   // Mark this as katalog-sync so we know we generated this
+					"external-source":    "kubernetes",                                               // Define the source of this service; see https://github.com/hashicorp/consul/blob/fc1d9e5d78749edc55249e5e7c1a8f7a24add99d/website/source/docs/platform/k8s/service-sync.html.md#service-meta
+					ConsulSyncSourceName: ConsulSyncSourceValue,                                      // Mark this as katalog-sync so we know we generated this
 					ConsulK8sLinkName:    podCacheKey(pod.ObjectMeta.Namespace, pod.ObjectMeta.Name), // which includes full path to this (ns, pod name, etc.)
 					ConsulK8sNamespace:   pod.ObjectMeta.Namespace,
 					ConsulK8sPod:         pod.ObjectMeta.Name,
